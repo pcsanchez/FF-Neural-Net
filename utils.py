87a1caa0fae0123daa95123bbc9de4067cfe90ad
Paco Sanchez <paco.sanchez.ram@gmail.com>
2020-12-01 12:13:51 +0000

    MinMax Normalization Utils

--- utils.py
+++ utils.py
@@ -8,3 +8,19 @@ def read_csv(file_name, num_inputs):
             X.append([float(n) for n in split_data[:num_inputs]])
             y.append([float(n) for n in split_data[num_inputs:]])
     return (X,y)
+
+# Apply minmax normalization to a matrix
+def minmax_normalize(data):
+    cols = len(data[0])
+    new_data = data.copy()
+    max_data = []
+    min_data = []
+    for col in range(cols):
+        col_values = [row[col] for row in data]
+        col_max = max(col_values)
+        col_min = min(col_values)
+        max_data.append(col_max)
+        min_data.append(col_min)
+        for row in new_data:
+            row[col] = (row[col] - col_min) / (col_max - col_min)
+    return (new_data, max_data, min_data)
